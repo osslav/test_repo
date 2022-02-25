d07c68ba0b05732e5e814df8ead9114d30d8d248
osslav <71839729+osslav@users.noreply.github.com>
2022-02-25 15:20:32 +0700

    add main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+
+class stack
+{
+public:
+    void pop();
+};
+
+struct apple
+{
+    int size;
+    char* color;
+};
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    return 0;//a.exec();
+}
